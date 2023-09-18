06a729df3dfecb9c0c613bc406aace74ee83c3d3
Jeromesam10 <143810392+Jeromesam10@users.noreply.github.com>
2023-09-18 14:21:30 +0530

    Add files via upload

---
+++ swap two numbres using c.cpp
@@ -0,0 +1,23 @@
+﻿// swap two numbres using c.cpp : Defines the entry point for the application.
+//
+
+#include<stdio.h>
+
+void swap(int, int);
+
+int main()
+{
+	int a, b;
+	printf("Enter Two Values:\n");
+	scanf("%d%d", &a, &b);
+	printf("Before swapping the values in main a=%d,b=%d\n", a, b);
+	swap(a, b);
+	printf("After swapping values in main a=%d,b=%d\n", a, b);
+}
+void swap(int a, int b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	printf("After swapping values in function a=%d,b=%d\n", a, b);
+}
